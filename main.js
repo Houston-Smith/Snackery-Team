eab4228c00d0e868ddfd3fa05255e4352ddf9bf0
LukeSlater02 <lukeslater101@gmail.com>
2022-02-18 10:43:08 -0600

    added a function that logs birthdays

---
+++ main.js
@@ -0,0 +1,8 @@
+let bDays = ["08-02-2001", "10-2-1991", "09-09-1994"];
+
+let logBDays = (bDays) => {
+    for (let i = 0; i < bDays.length; i++){
+        console.log(bDays[i]);
+    }
+}
+logBDays(bDays)
